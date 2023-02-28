c56521ad6a98b66a35254adb0ed29e4dc2843c6d
Eric Horodyski <horodyski@ionic.io>
2023-02-28 11:20:57 -0500

    initial commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  constructor(private http: HttpClient) {}
 
-  constructor() {}
-
+  ngOnInit() {
+    const url = 'https://jsonplaceholder.typicode.com/todos/1';
+    const headers = new HttpHeaders({
+      'X-Header-Test': 'test',
+      Authorization: 'my-auth-token',
+    });
+    this.http.get(url, { headers }).subscribe((res) => console.log(res));
+  }
 }
